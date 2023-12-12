6dc9b5ea8c9682616a238cca42a64ba83037b2f8
salty <salty@salty.dk>
2023-12-12 12:57:50 +0100

    less debug more config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 set -e
-set -x
-
-echo "Starting entrypoint script..."
 
 # Normalize the input for ENABLE_IPV6 to lowercase
-echo "Processing ENABLE_IPV6 environment variable..."
 ENABLE_IPV6_LOWER=$(echo "$ENABLE_IPV6" | tr '[:upper:]' '[:lower:]')
 
 # Check for different representations of 'true' and set BIND_CONFIG
@@ -17,27 +13,22 @@ case "$ENABLE_IPV6_LOWER" in
         BIND_CONFIG=":2375"
         ;;
 esac
-echo "BIND_CONFIG set to: $BIND_CONFIG"
 
 # Process the HAProxy configuration template
-echo "Generating HAProxy configuration..."
 envsubst < /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
-
 cat /usr/local/etc/haproxy/haproxy.cfg
 
-# first arg is `-f` or `--some-option`
+# Command-line argument handling
 if [ "${1#-}" != "$1" ]; then
     set -- haproxy "$@"
 fi
 
-# if no args or first arg is 'haproxy'
 if [ "$#" -eq 0 ] || [ "$1" = 'haproxy' ]; then
-    # if 'haproxy' is the first arg or no args, shift and prepend the default command
     if [ "$1" = 'haproxy' ]; then
         shift
     fi
     set -- haproxy -W -db -f /usr/local/etc/haproxy/haproxy.cfg "$@"
 fi
 
-echo "Executing command: $@"
 exec "$@"
+
