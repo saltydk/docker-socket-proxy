0da43b6127d673aeeba2d4724912aac27557cd70
salty <salty@salty.dk>
2024-01-31 17:19:34 +0100

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,27 +8,27 @@ DISABLE_IPV6_LOWER=$(echo "$DISABLE_IPV6" | tr '[:upper:]' '[:lower:]')
 case "$DISABLE_IPV6_LOWER" in
     1|true|yes)
         BIND_CONFIG=":2375"
+        echo "IPv6 Disabled"
         ;;
     *)
         BIND_CONFIG="[::]:2375 v4v6"
+        echo "IPv6 Enabled"
         ;;
 esac
 
 # Process the HAProxy configuration template using sed
 sed "s/\${BIND_CONFIG}/$BIND_CONFIG/g" /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 
-# first arg is `-f` or `--some-option`
+# Command-line argument handling
 if [ "${1#-}" != "$1" ]; then
-	set -- haproxy "$@"
+    set -- haproxy "$@"
 fi
 
-if [ "$1" = 'haproxy' ]; then
-	shift # "haproxy"
-	# if the user wants "haproxy", let's add a couple useful flags
-	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
-	#   -db -- disables background mode
-	set -- haproxy -W -db "$@"
+if [ "$#" -eq 0 ] || [ "$1" = 'haproxy' ]; then
+    if [ "$1" = 'haproxy' ]; then
+        shift
+    fi
+    set -- haproxy -W -db -f /usr/local/etc/haproxy/haproxy.cfg "$@"
 fi
 
 exec "$@"
-
