ccfd047c32d178f20cf50a3390e4d069b0934639
salty <salty@salty.dk>
2024-01-31 19:45:56 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,16 +19,17 @@ esac
 # Process the HAProxy configuration template using sed
 sed "s/\${BIND_CONFIG}/$BIND_CONFIG/g" /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
 
-# Command-line argument handling
+# first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
-    set -- haproxy "$@"
+	set -- haproxy "$@"
 fi
 
-if [ "$#" -eq 0 ] || [ "$1" = 'haproxy' ]; then
-    if [ "$1" = 'haproxy' ]; then
-        shift
-    fi
-    set -- haproxy -W -db -f /usr/local/etc/haproxy/haproxy.cfg "$@"
+if [ "$1" = 'haproxy' ]; then
+	shift # "haproxy"
+	# if the user wants "haproxy", let's add a couple useful flags
+	#   -W  -- "master-worker mode" (similar to the old "haproxy-systemd-wrapper"; allows for reload via "SIGUSR2")
+	#   -db -- disables background mode
+	set -- haproxy -W -db "$@"
 fi
 
 exec "$@"
